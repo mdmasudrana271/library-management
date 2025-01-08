a34b93820ca07dd064d4a22fe28bee1695129b17
mdmasudrana271 <mr7123619@gmail.com>
2025-01-09 01:36:32 +0600

    modify settings

--- library_managment/settings.py
+++ library_managment/settings.py
@@ -135,6 +135,9 @@ STATIC_URL = 'static/'
 
 STATICFILES_DIRS = [BASE_DIR / "static"]
 
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]  # Optional for development
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles") 
+
 
 
 
