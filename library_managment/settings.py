f236df567bed8482cf7f3f185ec05345febfb08c
mdmasudrana271 <mr7123619@gmail.com>
2025-01-09 01:54:25 +0600

    add port

--- library_managment/settings.py
+++ library_managment/settings.py
@@ -134,10 +134,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATICFILES_DIRS = [BASE_DIR / "static"]
+# STATICFILES_DIRS = [BASE_DIR / "static"]
+STATICFILES_DIRS = []
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]  # Optional for development
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles") 
 
 
 
